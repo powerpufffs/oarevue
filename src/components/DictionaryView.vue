30ae14a7adbeda6d79739ad8b071623538363da8
Brett Fisher <bfisher@simplenexus.com>
2019-07-04 20:05:38 -0600

    Added forms to dictionary view

--- src/components/DictionaryView.vue
+++ src/components/DictionaryView.vue
@@ -11,6 +11,18 @@
         {{ definition.definition }} 
       </li>
     </ul>
+
+
+    <h3 class="text-2xl text-gray-700 mt-3">Forms</h3>
+    <ul>
+      <li
+        v-for="(form, index) in forms"
+        :key="index"
+        class="text-gray-700"
+      >
+        {{ form.form }}: {{ spellingsList(form.spellings) }}
+      </li>
+    </ul>
     <p class="text-base text-gray-600"></p>
   </div>
 </template>
@@ -36,6 +48,13 @@ export default {
   },
 
   methods: {
+    spellingsList(spellingsArray) {
+      let spellings = []
+      spellingsArray.forEach(spelling => {
+        spellings.push(spelling.spelling)
+      })
+      return spellings.join(', ')
+    },
     async getWordInfo() { 
       let result = await axios.get('https://oare-test.herokuapp.com/api/dictionaryWords/'
         + this.wordId)
@@ -43,9 +62,16 @@ export default {
       console.log(data)
       this.word = data.word
       this.definitions = []
+      this.forms = []
       data.definitions.forEach(definition => {
         this.definitions.push({
           ...definition
+        })
+      })
+
+      data.forms.forEach(form => {
+        this.forms.push({
+          ...form
         })
       })
     }
