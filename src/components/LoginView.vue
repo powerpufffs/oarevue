f86f26dd289a9721a78d09aabef6733a38e05b21
Brett Fisher <bf6038@gmail.com>
2019-10-18 10:17:42 -0600

    Updated login and register routes

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,26 +1,11 @@
 <template>
-  <!-- <v-card class="mt-12 pa-3 mx-auto" max-width="600" elevation="24">
-    <v-card-title primary-title>
-      <h3 class="headline mb-2">Sign In</h3>
-    </v-card-title>
-    <v-card-text>
-      <v-text-field label="Email" outlined />
-      <v-text-field outlined v-model="password" label="Password" name="password" />
-      <v-btn text class="text-none" to="/register">Don't have an account? Register for free</v-btn>
-      <br />
-      <v-btn text class="text-none" to="/register">Forgot password?</v-btn>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn class="text-right" color="primary">Sign In</v-btn>
-    </v-card-actions>
-  </v-card>-->
   <OareUserCard title="Sign In">
     <template v-slot>
       <v-text-field label="Email" outlined />
       <v-text-field outlined v-model="password" label="Password" name="password" />
       <v-btn text class="text-none" to="/register">Don't have an account? Register for free</v-btn>
       <br />
-      <v-btn text class="text-none" to="/register">Forgot password?</v-btn>
+      <v-btn text class="text-none">Forgot password?</v-btn>
     </template>
 
     <template v-slot:actions>
--- src/components/RegisterView.vue
+++ src/components/RegisterView.vue
@@ -2,71 +2,25 @@
   <OareUserCard title="Register">
     <template v-slot>
       <form @submit.prevent="handleSubmit">
-        <v-col cols="12">
-          <v-text-field label="Firstname" outlined></v-text-field>
-        </v-col>
-        <v-col cols="12">
-          <v-text-field outlined v-model="user.lastname" label="Lastname" name="lastname"></v-text-field>
-        </v-col>
-        <v-col cols="12">
-          <v-text-field outlined v-model="user.email" label="Email" name="email"></v-text-field>
-        </v-col>
-        <v-col cols="12">
-          <v-text-field outlined v-model="user.password" label="Password" name="password"></v-text-field>
-        </v-col>
-        <v-col cols="12">
-          <v-text-field
-            outlined
-            v-model="user.repeatpassword"
-            label="Repeat Password"
-            name="repeatpassword"
-          ></v-text-field>
-        </v-col>
+        <v-text-field label="First Name" outlined></v-text-field>
+        <v-text-field outlined v-model="user.lastname" label="Last Name"></v-text-field>
+        <v-text-field outlined v-model="user.email" label="Email"></v-text-field>
+        <v-text-field outlined v-model="user.password" label="Password" type="password"></v-text-field>
+        <v-text-field
+          outlined
+          v-model="user.repeatpassword"
+          label="Confirm Password"
+          name="repeatpassword"
+          type="password"
+        ></v-text-field>
+        <v-btn text class="text-none" to="/login">Already have an account? Login</v-btn>
       </form>
     </template>
 
     <template v-slot:actions>
-      <v-btn color="primary">Register</v-btn>
+      <v-btn color="primary">Confirm</v-btn>
     </template>
   </OareUserCard>
-  <!-- <v-layout justify-space-between>
-    <v-flex xs10 sm6 offset-sm3>
-      <v-col>
-        <div>
-          <h3 class="headline mb-2 text-center">Sign Up</h3>
-        </div>
-        <form @submit.prevent="handleSubmit">
-          <v-col cols="12">
-            <v-text-field label="Firstname" outlined></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field outlined v-model="user.lastname" label="Lastname" name="lastname"></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field outlined v-model="user.email" label="Email" name="email"></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field outlined v-model="user.password" label="Password" name="password"></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field
-              outlined
-              v-model="user.repeatpassword"
-              label="Repeat Password"
-              name="repeatpassword"
-            ></v-text-field>
-          </v-col>
-          <v-row>
-            <v-col cols="12">
-              <v-flex>
-                <v-btn color="primary">Register</v-btn>
-              </v-flex>
-            </v-col>
-          </v-row>
-        </form>
-      </v-col>
-    </v-flex>
-  </v-layout>-->
 </template>
 
 <script>
